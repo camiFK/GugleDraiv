e797e6d8a927034bf6d213abba74550624c56666
camiit <camilaangelinatenorio@gmail.com>
2024-11-26 19:14:40 -0300

    Se modifico File para que almacene content como LONGTEXT

--- gugledraiv/src/main/java/com/draiv/gugledraiv/entities/File.java
+++ gugledraiv/src/main/java/com/draiv/gugledraiv/entities/File.java
@@ -1,8 +1,19 @@
 package com.draiv.gugledraiv.entities;
 
 import java.time.LocalDateTime;
-import java.util.*;
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class File {
@@ -29,7 +40,7 @@ public class File {
     private String mimeType;
 
     @Lob
-    @Column (name = "content")
+    @Column (name = "content", columnDefinition = "LONGTEXT")
     private String content;
 
     @Column(name = "fileHash", unique = true)
