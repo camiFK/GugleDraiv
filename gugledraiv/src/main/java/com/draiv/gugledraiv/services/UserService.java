a5b639ba2773883dd0ccbec11466ffb8aaea2230
camiit <camilaangelinatenorio@gmail.com>
2024-12-10 16:27:43 -0300

    Se modifico isAuthenticated para validar token consumiento API grupo 1

--- gugledraiv/src/main/java/com/draiv/gugledraiv/services/UserService.java
+++ gugledraiv/src/main/java/com/draiv/gugledraiv/services/UserService.java
@@ -1,7 +1,13 @@
 package com.draiv.gugledraiv.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.draiv.gugledraiv.entities.Users;
 import com.draiv.gugledraiv.repositories.UserRepository;
@@ -16,6 +22,42 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private static final String AUTHORIZE_URL = "https://poo-dev.unsada.edu.ar:8088/cuentas/API/isTokenValid";
+     
+    public boolean isAuthenticated(String token) {
+        if (token == null || token.isEmpty()) {
+            return false;
+        }
+        
+        try {
+            // Configurar headers con el token
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + token);
+
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(
+                AUTHORIZE_URL, HttpMethod.GET, entity, String.class
+            );
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                return true;
+            }
+    
+            if (response.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                return false;
+            }
+        } catch (Exception e){
+            return false;
+        }
+        return false;
+    }
+
+
+    /* 
     public boolean isAuthenticated(String token) {
         if (token == null || token.isEmpty()) {
             return false;
@@ -26,7 +68,7 @@ public class UserService {
         }
         return true;
     }
-
+    */ 
 
     public boolean deleteUserByToken(String token) {
         if (token == null || token.isEmpty()) {
